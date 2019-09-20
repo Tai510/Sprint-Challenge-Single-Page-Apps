12ee0e533bcfca74b04f5ca5fbcc0c2b0c21b377
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-09-20 09:45:42 -0700

    added styled component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
+import WelcomePage from './components/WelcomePage.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+    <Router>
+      <WelcomePage />
+      <Route exact path="/" component={CharacterList} />
+    </Router>
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,39 @@
 import React from "react";
+import styled, { css } from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharList = styled.div`
+  background: #93B169;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.12), 0 5px 2px rgba(0, 0, 0, 0.24);
+  width: 100%;
+  max-width: 400px;
+  padding: 10px;
+  margin: 16px 305px;
+  text-align: center;
+  color: white;
+  text-shadow: 1px 1px black;
+  height: 280px;
+  text-align: start;
+  font-family: 'Patrick Hand', cursive;
+`
+
+const Image = styled.img`
+  height: 80px;
+  width: 80px;
+  border: 1px solid black;
+  border-radius: 5rem;
+`
+
+export default function CharacterCard(props) {
+  // const { title, director, metascore, stars, id} = props.movie;
+
+
+  return (
+    <CharList className='char-list'>
+      <Image src={props.image}></Image>
+      <h1>Name : {props.name}</h1>
+      <h3> Status : {props.status}</h3>
+      <h3>Location: {props.location.name}</h3>
+      <h3>Origin: {props.origin.name}</h3>
+    </CharList>
+  )
 }
